8deb6d3753393b387107d5c95407f9697a7b7f6e
MisQLak <34073547+MisQLak@users.noreply.github.com>
2018-01-11 11:22:44 +0100

    Repaired screen orientation change.

--- app/src/main/java/pl/skylos/cognitivedisfunctionsystem/MainActivity.java
+++ app/src/main/java/pl/skylos/cognitivedisfunctionsystem/MainActivity.java
@@ -1,5 +1,6 @@
 package pl.skylos.cognitivedisfunctionsystem;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -12,22 +13,57 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Variables defined to call findViewById once.
+private TextView segment;
+private TextView segmentName;
+private TextView questionNum;
+private TextView cdsPointsText;
+private Button nextQ;
+private TextView questionAsk;
+private RadioGroup rg;
+private ProgressBar questNum;
+private ProgressBar cdsPoints;
+private static final String pointsGainedKey = "pointsGained";
+private static final String questionNumberKey = "questionNumber";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //finding view for later use.
         setContentView(R.layout.activity_main);
+        segment = findViewById(R.id.segment);
+        segmentName = findViewById(R.id.segment_desc);
+        questionNum = findViewById(R.id.question_number);
+        cdsPointsText = findViewById(R.id.cds_points);
+        nextQ = findViewById(R.id.next);
+        questionAsk = findViewById(R.id.question);
+        questNum = findViewById(R.id.questions_bar);
+        cdsPoints = findViewById(R.id.cds_bar);
+        rg = findViewById(R.id.q1);
         //Check saved data and returns it.
         if (savedInstanceState != null) {
-            pointsGained = savedInstanceState.getInt("pointsGained");
-            questionNumber = savedInstanceState.getInt("questionNumber");
-            TextView segment = findViewById(R.id.segment);
-            TextView segmentName = findViewById(R.id.segment_desc);
-            TextView questionNum = findViewById(R.id.question_number);
-            TextView cdsPointsText = findViewById(R.id.cds_points);
-            Button nextQ = findViewById(R.id.next);
+            pointsGained = savedInstanceState.getInt(pointsGainedKey);
+            questionNumber = savedInstanceState.getInt(questionNumberKey);
+            //if scores was displayed on screen - saved to display them.
+            if (questionNumber == 17) {
+                setContentView(R.layout.scores);
+                TextView finalScore = findViewById(R.id.final_score);
+                ProgressBar cds2 = findViewById(R.id.cds_bar2);
+                TextView scoreDesc = findViewById(R.id.score_desc);
+                finalScore.setText(String.valueOf(pointsGained));
+                cds2.setProgress(pointsGained);
+                //Set description for given scores.
+                if ( pointsGained < 8 ) {
+                    scoreDesc.setText(getString(R.string.points_normal));
+                } else if (pointsGained >= 8 && pointsGained <= 23 ) {
+                    scoreDesc.setText(getString(R.string.points_low));
+                } else if (pointsGained >= 24 && pointsGained <= 44 ) {
+                    scoreDesc.setText(getString(R.string.points_average));
+                } else if (pointsGained >= 45) {
+                    scoreDesc.setText(getString(R.string.points_high));
+                }
+            } else {
             String question = setQuestion(questionNumber);
-            TextView questionAsk = findViewById(R.id.question);
             questionAsk.setText(question);
             questionNum.setText(getString(R.string.question_number) + (questionNumber+1) + getString(R.string.of) + " 17");
             cdsPointsText.setText(getString(R.string.dog_results) + pointsGained);
@@ -44,47 +80,56 @@ public class MainActivity extends AppCompatActivity {
                     nextQ.setText(getString(R.string.result));
                 }
             }
-/*         positionX = savedInstanceState.getInt("position");
-           RadioGroup rg = findViewById(R.id.q1);
-            //This run only if any button was checked.
-            if (positionX != -1 ) {
-                ((RadioButton) rg.getChildAt(positionX)).setChecked(true);
-            }*/
+            }
         }
     }
-    //Saving data (radiobuttons state, questions, points, etc. state).
+    //Saving data (questions, points, etc. state).
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt("pointsGained", pointsGained);
-        outState.putInt("questionNumber", questionNumber);
-/*        //This run only if there was no score layout opened.
-        if (questionNumber <= 16) {
-        RadioGroup rg = findViewById(R.id.q1);
-            int id = rg.getCheckedRadioButtonId();
-            View radioB = rg.findViewById(id);
-            positionX = rg.indexOfChild(radioB);
-            outState.putInt("position", positionX);
-        }*/
+        outState.putInt(pointsGainedKey, pointsGained);
+        outState.putInt(questionNumberKey, questionNumber);
     }
     //variable holds number of points gained in quiz.
-    public int pointsGained = 0;
+    private int pointsGained = 0;
     //variable setting correct question from array setQuestion.
-    public int questionNumber = 0;
-    //variable set which radiobutton is checked.
-    public int positionX;
+    private int questionNumber = 0;
+    //variable stores data for last gained points.
+/*    public int lastPoints;
+
+    public void onBackPressed() {
+        if (questionNumber > 0 && questionNumber < 17 ) {
+            pointsGained -= lastPoints;
+            questionNumber -= 1;
+            String question = setQuestion(questionNumber);
+            questionAsk.setText(question);
+            points();
+            rg.clearCheck();
+            questionNum.setText(getString(R.string.question_number) + (questionNumber+1) + getString(R.string.of) + " 17");
+            cdsPointsText.setText(getString(R.string.dog_results) + pointsGained);
+            questNum.setProgress(questionNumber);
+            cdsPoints.setProgress(pointsGained);
+            //changing headers depends of question
+            if (questionNumber > 4 && questionNumber < 10) {
+                segment.setText("B");
+                segmentName.setText(getString(R.string.segment_B));
+            } else if (questionNumber > 9 && questionNumber < 12) {
+                segment.setText("C");
+                segmentName.setText(getString(R.string.segment_C));
+            } else if (questionNumber > 11) {
+                segment.setText("D");
+                segmentName.setText(getString(R.string.segment_D));
+            }
+        } else if (questionNumber == 17) {
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+        }
+    }*/
 
 
     //Method which makes button "next" working as intended.
     public void nextQuestion(View view) {
-        TextView segment = findViewById(R.id.segment);
-        TextView segmentName = findViewById(R.id.segment_desc);
-        TextView questionNum = findViewById(R.id.question_number);
-        TextView cdsPointsText = findViewById(R.id.cds_points);
-        RadioGroup rg = findViewById(R.id.q1);
-        ProgressBar questNum = findViewById(R.id.questions_bar);
-        ProgressBar cdsPoints = findViewById(R.id.cds_bar);
-        Button nextQ = findViewById(R.id.next);
         //statement check is there any button checked, if not do nothing.
         if (rg.getCheckedRadioButtonId() == -1) {
         } else {
@@ -92,22 +137,25 @@ public class MainActivity extends AppCompatActivity {
             questionNumber += 1;
                 //run if it is the one before last question
             if (questionNumber >= 16) {
-                nextQ.setText(getString(R.string.result));
-                points();
-                rg.clearCheck();
-                questionNum.setText(getString(R.string.question_number) + (questionNumber+1) + getString(R.string.of) + " 17");
-                cdsPointsText.setText(getString(R.string.dog_results) + pointsGained);
-                questNum.setProgress(questionNumber);
-                cdsPoints.setProgress(pointsGained);
-                    //run if it is last question, showing results
+                //run if it is last question, showing results
                 if ( questionNumber > 16 ) {
+                    points();
                     score(view);
+                } else {
+                    String question = setQuestion(questionNumber);
+                    questionAsk.setText(question);
+                    nextQ.setText(getString(R.string.result));
+                    points();
+                    rg.clearCheck();
+                    questionNum.setText(getString(R.string.question_number) + (questionNumber + 1) + getString(R.string.of) + " 17");
+                    cdsPointsText.setText(getString(R.string.dog_results) + pointsGained);
+                    questNum.setProgress(questionNumber);
+                    cdsPoints.setProgress(pointsGained);
                 }
 
             } else {
                 //updating question to next one, clearing radiobuttons, updating progress bars
                 String question = setQuestion(questionNumber);
-                TextView questionAsk = findViewById(R.id.question);
                 questionAsk.setText(question);
                 points();
                 rg.clearCheck();
@@ -154,44 +202,53 @@ public class MainActivity extends AppCompatActivity {
     }
     //This method add points depends of which button is checked.
     public int points () {
-        RadioGroup rg = findViewById(R.id.q1);
         int id = rg.getCheckedRadioButtonId();
         View radioB = rg.findViewById(id);
         int position = rg.indexOfChild(radioB);
 
         switch( position ) {
             case 0:
+                lastPoints = 0;
                 pointsGained += 0;
                 break;
             case 1:
                 if (questionNumber == 10 || questionNumber == 11) {
                 pointsGained += 4;
+                lastPoints = 4;
                 } else {
                 pointsGained += 2;
+                lastPoints = 2;
                 }
                 break;
             case 2:
                 if (questionNumber == 10 || questionNumber == 11) {
                     pointsGained += 6;
+                    lastPoints = 6;
                 } else {
                     pointsGained += 3;
+                    lastPoints = 3;
                 }
                 break;
             case 3:
                 if (questionNumber == 10 || questionNumber == 11) {
                     pointsGained += 8;
+                    lastPoints = 8;
                 } else {
                     pointsGained += 4;
+                    lastPoints = 2;
                 }
                 break;
             case 4:
                 if (questionNumber == 10 || questionNumber == 11) {
                     pointsGained += 10;
+                    lastPoints = 10;
                 } else {
                     pointsGained += 5;
+                    lastPoints = 5;
                 }
                 break;
         }
+        lastPoints += 0;
         return pointsGained;
     }
     //Method restarts quiz.
